5e647c15f735c9866353ed4d11b698718d42c076
Siradji Awoual <siradjiawoual@gmail.com>
2020-06-01 10:24:07 +0000

    user register routes

---
+++ classified-app/src/components/Root/Root.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const Root = () => {
+    return (
+
+        <Wrapper>
+          <Container>
+            <Content>
+                content
+            </Content>
+            <Sidebar>
+                sidebar
+            </Sidebar>
+          </Container>
+        </Wrapper>
+    )
+}
+
+
+
+const Container = styled.div`
+display: flex;
+flex-flow: row nowrap;
+margin: auto;
+width: 80rem;
+`
+const Content  = styled.div`
+flex: 1;
+margin-right: 1rem;
+`
+const Sidebar = styled.div`
+flex: 0 auto;
+width: 10rem;
+`
+
+const Wrapper =  styled.div`
+box-sizing: border-box;
+height: 100vh;
+padding:  1rem;
+width: 100%;  
+
+`
+export default Root
